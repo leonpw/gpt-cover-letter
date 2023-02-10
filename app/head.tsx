cfd620d26cc76351204a677545993a74977e61bb
leon <git@wieisleon.nl>
2023-02-10 12:03:56 +0200

    thumbnail and some og for fb/twitter

--- app/head.tsx
+++ app/head.tsx
@@ -5,6 +5,20 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Write a cover letter based on the job description by GPT3" />
       <link rel="icon" href="/favicon.ico" />
+     
+      <meta property="og:url" content="https://cover-letter.wieisleon.nl/"/>
+      <meta property="og:type" content="website"/>
+      <meta property="og:title" content="Write me a cover letter"/>
+      <meta property="og:description" content="Write a cover letter based on the job description by GPT3"/>
+      <meta property="og:image" content="https://cover-letter.wieisleon.nl/thumbnail.png"/>
+
+      <meta name="twitter:card" content="summary_large_image"/>
+      <meta property="twitter:domain" content="cover-letter.wieisleon.nl"/>
+      <meta property="twitter:url" content="https://cover-letter.wieisleon.nl/"/>
+      <meta name="twitter:title" content="Write me a cover letter"/>
+      <meta name="twitter:description" content="Write a cover letter based on the job description by GPT3"/>
+      <meta name="twitter:image" content="https://cover-letter.wieisleon.nl/thumbnail.png"/>
+
     </>
   )
 }
